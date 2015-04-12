92f8f385984633094007fe6fc94db89af17a4fbe
embren <embren@users.noreply.github.com>
2015-04-12 17:32:46 +0100

    Create Plot3.R

---
+++ Plot3.R
@@ -0,0 +1,10 @@
+setwd("C:\\Users\\A580489\\Documents\\R\\Coursera\\Exploratory Data Analysis\\exdata-data-household_power_consumption")
+data <- read.table("household_power_consumption.txt",sep=";", header=T,stringsAsFactors=F)
+data$Date <- as.Date(data$Date, format = "%d/%m/%Y") 
+data2 <- data[c(which(data$Date=="2007-02-01"),which(data$Date=="2007-02-02")),]
+data2$DateTime <- strptime(paste(data2$Date, data2$Time, sep=" "), format = "%Y-%m-%d %H:%M:%S")
+png("Plot3.png",width=480,height=480)
+plot(data2$DateTime, data2$Sub_metering_1, type="l",col = "black",xlab="",ylab="Energy Sub Metering" )
+lines(data2$DateTime,data2$Sub_metering_2, type="l",col = "red")
+lines(data2$DateTime,data2$Sub_metering_3, type="l",col = "blue")
+dev.off()
